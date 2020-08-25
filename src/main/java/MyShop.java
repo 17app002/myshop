61e75bb2cc3f1afeab602a1962de136df5601842
iiiplay001@gmail.com <Me09235168>
2020-08-25 13:03:36 +0800

    完成登入介面

--- src/main/java/MyShop.java
+++ src/main/java/MyShop.java
@@ -1,21 +1,36 @@
 import dbutil.DBUtil;
 
+
 public class MyShop {
 
-    private String dbName="myshop";
-    private String tableName="admin";
+    private String dbName = "myshop";
+    public static DBUtil dbUtil;
 
-    public MyShop(){
+    public LoginPage loginPage;
 
-        DBUtil dbUtil=new DBUtil(dbName,"root","password");
-
-        if(dbUtil.isConnection()){
-            dbUtil.release();
+    public MyShop() {
+        dbUtil = new DBUtil(dbName, "root", "password");
+        if (dbUtil.isConnection()) {
+            System.out.println("商店資料初始化成功....");
         }
     }
 
+    public boolean login() {
+        loginPage = new LoginPage();
+        return loginPage.isLogin();
+    }
+
     public static void main(String[] args) {
-        MyShop myShop=new MyShop();
+        MyShop myShop = new MyShop();
+
+        if (!myShop.login()) {
+            System.out.println("登出系統成功");
+            dbUtil.release();
+            return;
+        }
+
+        System.out.println("登入成功!");
+        dbUtil.release();
     }
 
 }
